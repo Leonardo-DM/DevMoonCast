9ed974a5003608497a70af63eacde6cb01722aa4
DevMoon <69600417+daluatv@users.noreply.github.com>
2022-05-24 16:08:57 -0300

    Update data.js

--- data.js
+++ data.js
@@ -10,51 +10,75 @@ export default [
     title:
       "GP DA ZL, MC Vitinho Avassalador - Automotivo Embrazante ( DJ L30 SC ) ",
     artist: "Dj L30 SC",
-    cover: "capa-l30sc.png",
+    cover: "loffi.jpg",
     file: "GP DA ZL, MC Vitinho Avassalador - Automotivo Embrazante.mp3"
   },
   {
     title:
       "DJ Shalom, mc jhenny, MC Yuri, MC Marsha, MC P1, GP DA ZL - Fds Tô Na Rave ( DJ L30 SC ) ",
     artist: "Dj L30 SC",
-    cover: "capa-l30sc.png",
+    cover: "loffi.jpg",
     file: "DJ Shalom, mc jhenny, MC Yuri, MC Marsha, MC P1, GP DA ZL - Fds Tô Na Rave.mp3"
   },
   {
     title:
-      "OH Mãe como a Isabella ta no Baile ( DJ L30 SC ) ",
-    artist: "Dj L30 SC",
-    cover: "capa-l30sc.png",
-    file: "OH-Mãe-como-a-Isabella-ta-no-Baile.mp3"
+      "a girl and a cat - rainbow bridge",
+    artist: "Rainbow Bridge",
+    cover: "loffi.jpg",
+    file: "a girl and a cat - rainbow bridge.mp3"
   },
   {
-    title: "DJ ERY 2 - O MAESTRO DOS FLUXOS 2 ( Dj L30 SC )",
-    artist: "Dj L30 SC",
-    cover: "capa-l30sc.png",
-    file: "O-MAESTRO-DOS-FLUXOS-2.mp3"
+    title: "Altair Blake - Cloudy",
+    artist: "Cloudy",
+    cover: "loffi.jpg",
+    file: "Altair Blake - Cloudy.mp3"
   },
   {
-    title: "AI AI BOTA BOTA BOTA - VEM NA VARA ( DJ L30 SC )",
-    artist: "Dj L30 SC",
-    cover: "capa-l30sc.png",
-    file: "AI-AI-BOTA-BOTA-BOTA-VEM-NA-VARA.mp3"
+    title: "Chilled Cougar - 435Hz",
+    artist: "435Hz",
+    cover: "loffi.jpg",
+    file: "Chilled Cougar - 435Hz.mp3"
   },
   {
-    title: "DJ Ery - Montagem dos Relíquias Furacão 2000 ( DJ L30 SC )",
-    artist: "Dj L30 SC",
-    cover: "capa-l30sc.png",
-    file: "Montagem-dos-Relíquias-Furacão-2000.mp3"
+    title: "G Mills - Lush",
+    artist: "Lush",
+    cover: "loffi.jpg",
+    file: "G Mills - Lush.mp3"
   },
   {
-    title: "VEM PRO BAILE DO BEGA ( DJ L30 SC )",
-    artist: "Dj L30 SC",
-    cover: "capa-l30sc.png",
-    file: "VEM-PRO-BAILE-DO-BEGA.mp3"
+    title: "Kupla - Kingdom in Blue",
+    artist: "Kupla",
+    cover: "loffi.jpg",
+    file: "Kupla - Kingdom in Blue.mp3"
   },
   {
-    title: "OLHA SO PIRANHA OQUE EU VOU TI DIZER  - SENTA NA MINHA PIKA 123 ( DJ L30 SC )",
-    artist: "Dj L30 SC",
-    cover: "capa-l30sc.png",
-    file: "SENTA-NA-MINHA-PIKA-123.mp3"
+    title: "Kupla - Roots",
+    artist: "Kupla",
+    cover: "loffi.jpg",
+    file: "Kupla - Roots.mp3"
+  },
+  {
+    title: "Purrple Cat - Sugar Coat",
+    artist: "Purrple Cat",
+    cover: "loffi.jpg",
+    file: "Purrple Cat - Sugar Coat.mp3"
+  },
+  {
+    title: "Rook1e - I Fell in Love with You One Night in September",
+    artist: "Rook1e",
+    cover: "loffi.jpg",
+    file: "Rook1e - I Fell in Love with You One Night in September.mp3"
+  },
+  {
+    title: "Saib - Sakura Trees",
+    artist: "Saib",
+    cover: "loffi.jpg",
+    file: "Saib - Sakura Trees.mp3"
+  },
+  {
+    title: "Shierro - Coffee Camino",
+    artist: "Shierro",
+    cover: "loffi.jpg",
+    file: "Shierro - Coffee Camino.mp3"
   }
 ];
