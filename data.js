8aec765c79cf57dee1a2f8f86760745562588de9
DevMoon <69600417+daluatv@users.noreply.github.com>
2022-05-24 15:57:09 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,60 @@
+export default [
+  {
+    title:
+      "ELA MAMA MAMA, MAMA - MC BIANO DO IMPÉRA + DJ L30 SC ",
+    artist: "Dj L30 SC",
+    cover: "ElaMama-capa-l30sc.png",
+    file: "ELA MAMA MAMA, MAMA - MC BIANO DO IMPÉRA + DJ L30 SC.mp3"
+  },
+  {
+    title:
+      "GP DA ZL, MC Vitinho Avassalador - Automotivo Embrazante ( DJ L30 SC ) ",
+    artist: "Dj L30 SC",
+    cover: "capa-l30sc.png",
+    file: "GP DA ZL, MC Vitinho Avassalador - Automotivo Embrazante.mp3"
+  },
+  {
+    title:
+      "DJ Shalom, mc jhenny, MC Yuri, MC Marsha, MC P1, GP DA ZL - Fds Tô Na Rave ( DJ L30 SC ) ",
+    artist: "Dj L30 SC",
+    cover: "capa-l30sc.png",
+    file: "DJ Shalom, mc jhenny, MC Yuri, MC Marsha, MC P1, GP DA ZL - Fds Tô Na Rave.mp3"
+  },
+  {
+    title:
+      "OH Mãe como a Isabella ta no Baile ( DJ L30 SC ) ",
+    artist: "Dj L30 SC",
+    cover: "capa-l30sc.png",
+    file: "OH-Mãe-como-a-Isabella-ta-no-Baile.mp3"
+  },
+  {
+    title: "DJ ERY 2 - O MAESTRO DOS FLUXOS 2 ( Dj L30 SC )",
+    artist: "Dj L30 SC",
+    cover: "capa-l30sc.png",
+    file: "O-MAESTRO-DOS-FLUXOS-2.mp3"
+  },
+  {
+    title: "AI AI BOTA BOTA BOTA - VEM NA VARA ( DJ L30 SC )",
+    artist: "Dj L30 SC",
+    cover: "capa-l30sc.png",
+    file: "AI-AI-BOTA-BOTA-BOTA-VEM-NA-VARA.mp3"
+  },
+  {
+    title: "DJ Ery - Montagem dos Relíquias Furacão 2000 ( DJ L30 SC )",
+    artist: "Dj L30 SC",
+    cover: "capa-l30sc.png",
+    file: "Montagem-dos-Relíquias-Furacão-2000.mp3"
+  },
+  {
+    title: "VEM PRO BAILE DO BEGA ( DJ L30 SC )",
+    artist: "Dj L30 SC",
+    cover: "capa-l30sc.png",
+    file: "VEM-PRO-BAILE-DO-BEGA.mp3"
+  },
+  {
+    title: "OLHA SO PIRANHA OQUE EU VOU TI DIZER  - SENTA NA MINHA PIKA 123 ( DJ L30 SC )",
+    artist: "Dj L30 SC",
+    cover: "capa-l30sc.png",
+    file: "SENTA-NA-MINHA-PIKA-123.mp3"
+  }
+];
